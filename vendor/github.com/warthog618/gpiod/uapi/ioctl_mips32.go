21104ea6d401c6d21be032d08a72644bfa947428
edgex-jenkins <collab-it+edgex@linuxfoundation.org>
2025-03-12 07:46:32 +0000

    ci(lts-release): LTS release v4.0.0 @99341b5

---
+++ vendor/github.com/warthog618/gpiod/uapi/ioctl_mips32.go
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2020 Kent Gibson <warthog618@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+//go:build mips || mipsle || mips64 || mips64le || ppc64 || ppc64le || sparc || sparc64
+// +build mips mipsle mips64 mips64le ppc64 ppc64le sparc sparc64
+
+package uapi
+
+// ioctl constants
+const (
+	iocNRBits    = 8
+	iocTypeBits  = 8
+	iocDirBits   = 3
+	iocSizeBits  = 13
+	iocNRShift   = 0
+	iocTypeShift = iocNRShift + iocNRBits
+	iocSizeShift = iocTypeShift + iocTypeBits
+	iocDirShift  = iocSizeShift + iocSizeBits
+	iocWrite     = 4
+	iocRead      = 2
+	// iocNone = 1
+)
